b10e717e80394065dfa56bbe24d2b06bd0132a3a
EitanMohorade <eitanluc@gmail.com>
2023-05-22 16:30:04 -0300

    keep going

--- lib/functions/example.dart
+++ lib/functions/example.dart
@@ -6,10 +6,13 @@ class Example extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          title: const Text('data'),
+        ),
+        body: const Center(
           child: ExampleParallax(),
         ),
       ),
--- lib/layouts/navBar.dart
+++ lib/layouts/navBar.dart
@@ -101,16 +101,5 @@ class _MyHomePageState extends State<MyHomePageState> {
 Route _createRoute(Widget widget) {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => widget,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(0.0, 1.0);
-      const end = Offset.zero;
-      final tween = Tween(begin: begin, end: end);
-      final offsetAnimation = animation.drive(tween);
-
-      return SlideTransition(
-        position: offsetAnimation,
-        child: child,
-      );
-    },
   );
 }
